be31ce9ea8be4832217ab8b86bbd34beab6c6a8c
Thomas Kim <thomastkim8@gmail.com>
2016-06-20 15:18:43 -0700

    Updating service provider to reflect latest Laravel changes

--- src/Kim/Activity/ActivityServiceProvider.php
+++ src/Kim/Activity/ActivityServiceProvider.php
@@ -44,10 +44,11 @@ class ActivityServiceProvider extends ServiceProvider
     {
         $connection = $this->app['config']['session.connection'];
         $table = $this->app['config']['session.table'];
+        $lifetime = $this->app['config']['session.lifetime'];
 
-        $this->app['session']->extend('database', function($app) use ($connection, $table)
+        $this->app['session']->extend('database', function($app) use ($connection, $table, $lifetime)
         {
-            return new DatabaseWithUserSessionHandler($this->app['db']->connection($connection), $table);
+            return new DatabaseWithUserSessionHandler($this->app['db']->connection($connection), $table, $lifetime);
         });
     }
 }
